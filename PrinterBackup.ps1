cad683b88207343035d95b7639081e860ccc78d5
alexslssp <alex.s.sousa@gmail.com>
2016-05-16 15:20:43 -0300

    Create PrinterBackup.ps1

---
+++ PrinterBackup.ps1
@@ -0,0 +1,13 @@
+$momento=get-date -uformat "%Y_%m_%d"
+foreach ($server in get-content c:\printservers.ini) {
+#Lista simples dentro do arquivo ini
+C:\Windows\System32\spool\tools\PrintBrm.exe -B -S $server -F c:\BAckup_servers\BKP_"$server"_"$momento"
+if ($error -ne $null){
+    Send-MailMessage `
+    -From ti.infra@cdb.com.br `
+    -To ti.infra@cdb.com.br `
+    -Subject "Erros no backup dos servidores de impressão" `
+    -Body "$error" `
+    -SmtpServer 172.21.20.21
+    }
+}
